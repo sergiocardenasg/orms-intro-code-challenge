7ed65c8fb584357de88efe17c5227d9347fff0cf
allisonkadel <akadel01@gmail.com>
2020-09-29 17:26:52 -0400

    Update agent.rb

--- lib/agent.rb
+++ lib/agent.rb
@@ -2,13 +2,13 @@ class Agent
     # Agent has many Listings
     attr_reader :name
     attr_accessor :telephone, :yrs_experience
-  ​
+  
     def initialize(name, telephone, yrs_experience)
       @name = name 
       @telephone = telephone 
       @yrs_experience = yrs_experience
     end   
-  ​
+  
     def listings 
       Listing.all.select {|listing| listing.agent == self}
     end   
