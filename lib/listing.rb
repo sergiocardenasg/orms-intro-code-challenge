920e5f51fcd7678da05d7c52e4c05da4c5ac6564
allisonkadel <akadel01@gmail.com>
2020-09-29 17:26:26 -0400

    Update listing.rb

--- lib/listing.rb
+++ lib/listing.rb
@@ -2,20 +2,20 @@ class Listing
     # Listing belongs to Agent
     attr_reader :location
     attr_accessor :status, :price, :agent
-​
+
     @@all = []
-​
+
     def initialize(location, price, status="for sale")
         @location = location
         @price = price
         @status = status
         @@all << self
     end
-​
+
     def price_drop(percentage)
         self.price -= self.price*percentage
     end
-​
+
     def self.all
         @@all
     end
